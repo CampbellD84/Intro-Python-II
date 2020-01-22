6cef666ab8a79b429d1f6bb3e578e9d03bb186a7
Douglas Campbell <dacampbelltbone@gmail.com>
2020-01-21 23:27:46 -0500

    Create Player and Room objects. Initial attempt at getting gplayermoving in game. More work is needed.

--- src/player.py
+++ src/player.py
@@ -1,2 +1,13 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, player_name, curr_location):
+        self.player_name = player_name
+        self.curr_location = curr_location
+
+    def __str__(self):
+        return f'\
+                {self.player_name.capitalize()} is currently\
+                     in or at the {self.curr_location}'
